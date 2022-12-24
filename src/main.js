8f11f63bc1b49e09061597e4a119473117842217
xiaobin <481996467@qq.com>
2022-12-25 01:33:52 +0800

    小案列

---
+++ src/main.js
@@ -0,0 +1,12 @@
+// 引入简易版vue
+import Vue from 'vue'
+// 引入App组件
+import App from './App.vue'
+
+// 以阻止 vue 在启动时生成生产提示
+Vue.config.productionTip = false
+
+// 绑定组件
+new Vue({
+    render: h => h(App)
+}).$mount('#app')
