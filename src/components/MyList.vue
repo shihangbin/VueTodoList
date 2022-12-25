d7c7cdc81bb29cb46c9dd362335c3e7c4a391eca
xiaobin <67466410+ixiaobin@users.noreply.github.com>
2022-12-25 13:43:57 +0800

    Add files via upload

--- src/components/MyList.vue
+++ src/components/MyList.vue
@@ -1,32 +1,32 @@
 <template>
-  <ul class="todo-main">
-    <MyTodo></MyTodo>
-  </ul>
+	<ul class="todo-main">
+		<MyTodo></MyTodo>
+	</ul>
 </template>
 
 <script>
-import MyTodo from "./MyTodo.vue";
-export default {
-  name: "MyList",
-  components: { MyTodo },
-};
+	import MyTodo from './MyTodo.vue'
+	export default {
+		name: 'MyList',
+		components: { MyTodo },
+	}
 </script>
 
 <style scoped>
-/*main*/
-.todo-main {
-  margin-left: 0px;
-  border: 1px solid #ddd;
-  border-radius: 2px;
-  padding: 0px;
-}
+	/*main*/
+	.todo-main {
+		margin-left: 0px;
+		border: 1px solid #ddd;
+		border-radius: 2px;
+		padding: 0px;
+	}
 
-.todo-empty {
-  height: 40px;
-  line-height: 40px;
-  border: 1px solid #ddd;
-  border-radius: 2px;
-  padding-left: 5px;
-  margin-top: 10px;
-}
+	.todo-empty {
+		height: 40px;
+		line-height: 40px;
+		border: 1px solid #ddd;
+		border-radius: 2px;
+		padding-left: 5px;
+		margin-top: 10px;
+	}
 </style>
